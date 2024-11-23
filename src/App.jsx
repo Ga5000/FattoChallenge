6edc4bda99b698ac010041c6962f0f5f95ac6c2b
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-11-22 21:44:21 -0300

    added more information in the alert at the start of the application

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada.");
+    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada. Caso as tarefas não carreguem, talvez seja necessário recarregar a página várias vezes.");
   }, []);
 
   return (
-    <>
+    <div>
       <Tasks />
-    </>
+    </div>
   );
 }
 
