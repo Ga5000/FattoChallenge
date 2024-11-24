2eb8fe3e3e263a431524c3c0f548b8656160bc0f
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-11-24 19:48:07 -0300

    updated alert message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada. Caso as tarefas não carreguem, talvez seja necessário recarregar a página várias vezes.");
+    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada. Pode ser que o seu antivirus esteja bloqueando as requests, abra o console e acesse a url da api, assim o problema sera corrigido");
   }, []);
 
   return (
