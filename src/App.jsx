cbc332e305775ab0259aada37536494aabf6ffbf
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-11-25 12:13:30 -0300

    updated alert message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada. Pode ser que o seu antivirus esteja bloqueando as requests, abra o console e acesse a url da api, assim o problema sera corrigido");
+    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada. Pode ser que o seu antivirus esteja bloqueando as requests, abra o console e acesse a url da api ('https://44.210.114.62:443'), assim o problema será corrigido");
   }, []);
 
   return (
