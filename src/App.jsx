89576edd6ffc97efec80ac14c17808d784e32fce
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-11-26 13:18:59 -0300

    alert message update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada.");
+    alert("Para mover uma tarefa entre páginas, basta arrastar a tarefa e mantê-la sobre o botão de próxima página (>) até a página mudar. Em seguida, solte-a na posição desejada. A API PODE DEMORAR ALGUM TEMPO PARA RESPONDER AS REQUISIÇÕES POR CONTA DA POLITICA DO RENDER.COM");
   }, []);
 
   return (
